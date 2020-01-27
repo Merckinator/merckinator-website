0faad659b5697d5820a0374a723d649d20857e91
Jeff Merckens <jeff.merckens@gmail.com>
2020-01-26 21:10:48 -0500

    Add Blaze HTML rendering

--- app/Main.hs
+++ app/Main.hs
@@ -5,11 +5,14 @@ import Lib
 import System.Environment (getEnv)
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
-import Web.Scotty
+import Text.Blaze.Html.Renderer.Text
+import Web.Scotty as S
+
 
 main :: IO ()
 main = do
     port <- getEnv "PORT"
     scotty (read port) $ do
         notFound $ do
-            html "<h1>Coming soon!</h1>"
+            S.html . renderHtml $ do
+                H.h1 "Coming soon!"
