ff117845b778d7f08bef06c7f1564559406dcf72
Jeff Merckens <jeff.merckens@gmail.com>
2020-01-26 20:57:32 -0500

    Get app working in Heroku

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lib
+import System.Environment (getEnv)
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+import Web.Scotty
 
 main :: IO ()
-main = someFunc
+main = do
+    port <- getEnv "PORT"
+    scotty (read port) $ do
+        notFound $ do
+            html "<h1>Coming soon!</h1>"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib
     ( someFunc
     ) where
