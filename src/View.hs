23697570b5aad29fe114816f0f198c710c59fb61
Jeff Merckens <jeff.merckens@gmail.com>
2020-01-28 05:49:57 -0500

    Split view out of lib

---
+++ src/View.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+module View
+    ( comingSoon
+    , html5Boilerplate
+    ) where
+
+import Text.Blaze.Html5 as H
+import Text.Blaze.Html5.Attributes as A
+import StyleSheet
+
+comingSoon :: Html
+comingSoon = do
+    html5Boilerplate "Under Construction" $ do
+                    H.div ! A.id "container" $
+                        h1 "Coming soon!"
+
+
+html5Boilerplate :: String -> Html -> Html
+html5Boilerplate title body = do
+    docTypeHtml $ do
+        H.head $ do
+            meta ! httpEquiv "X-UA-Compatible" ! content "IE=edge"
+            meta ! charset "utf-8"
+            if title == "home" then
+                H.title "Merckinator.com"
+            else
+                H.title . toHtml $ "Merckinator.com - " ++ title
+            meta ! name "description" ! content "A personal website trying out different tech"
+            meta ! name "author" ! content "Jeff Merckens"
+            meta ! name "viewport" ! content "width=device-width, initial-scale=1"
+            H.style $ toHtml globalStyleSheet
+            link ! rel "shortcut icon" ! href "#"
+        H.body body
